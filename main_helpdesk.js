1107d3519402d64addb2bf81d683fb5a268857c9
alanselivanov <alan.selivanov.mail@gmail.com>
2025-01-07 20:56:13 +0600

    added a profile part

--- main_helpdesk.js
+++ main_helpdesk.js
@@ -19,3 +19,35 @@ document.getElementById('supportForm').addEventListener('submit', async function
         alert('Error sending message');
     }
 });
+
+window.onload = function() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    
+    if (user) {
+        document.getElementById('login-btn').innerText = 'Logout';
+        document.getElementById('profile-btn').innerText = 'Profile';
+
+        document.getElementById('login-btn').onclick = function(e) {
+            e.preventDefault(); 
+            localStorage.removeItem('user');
+            window.location.href = '/static/login_page.html';
+        };
+
+        document.getElementById('profile-btn').onclick = function(e) {
+            e.preventDefault();
+            window.location.href = '/static/profile.html';
+        };
+    } else {
+        document.getElementById('login-btn').innerText = 'Login';
+        document.getElementById('profile-btn').innerText = 'Sign-up';
+
+        document.getElementById('login-btn').onclick = function() {
+            window.location.href = '/static/login_page.html';
+        };
+
+        document.getElementById('profile-btn').onclick = function() {
+            window.location.href = '/static/signup_page.html';
+        };
+    }
+};
+
